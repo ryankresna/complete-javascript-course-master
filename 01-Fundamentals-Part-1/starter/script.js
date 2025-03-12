3bf3752e3c787b4b2c5ee75c39c8ed95bd1346ce
Ryan_Kresna <ryankresna.n@gmail.com>
2025-03-12 22:49:23 +0700

    javascript

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,129 +1,163 @@
-// // /*
-// // let js = "amazing";
-// // console.log( 40 + 8 + 23 + 10 );
+// // // /*
+// // // let js = "amazing";
+// // // console.log( 40 + 8 + 23 + 10 );
 
-// // console.log( "Jonas" );
-// // console.log( 23 );
+// // // console.log( "Jonas" );
+// // // console.log( 23 );
 
-// // let firstName = "Jonas";
+// // // let firstName = "Jonas";
 
-// // console.log( firstName, js );
-// // console.log( firstName, js );
-// // console.log( firstName, js );
+// // // console.log( firstName, js );
+// // // console.log( firstName, js );
+// // // console.log( firstName, js );
 
-// // let jonas_matilda = "JM";
-// // let $function = 27;
+// // // let jonas_matilda = "JM";
+// // // let $function = 27;
 
-// // let person = "jonas";
-// // let PI = 3.1415;
+// // // let person = "jonas";
+// // // let PI = 3.1415;
 
-// // let myFirstJob = "Coder";
-// // let myCurrentJob = "Teacher";
+// // // let myFirstJob = "Coder";
+// // // let myCurrentJob = "Teacher";
 
-// // let job1 = "programmer";
-// // let job2 = "teacher";
+// // // let job1 = "programmer";
+// // // let job2 = "teacher";
 
-// // console.log( myFirstJob );
-// // */
+// // // console.log( myFirstJob );
+// // // */
 
 
-// // let javascriptIsFun = true;
-// // console.log( javascriptIsFun );
+// // // let javascriptIsFun = true;
+// // // console.log( javascriptIsFun );
 
-// // // console.log( typeof true );
-// // console.log( typeof javascriptIsFun );
-// // // console.log( typeof 23 );
-// // // console.log( typeof "Jonas" );
+// // // // console.log( typeof true );
+// // // console.log( typeof javascriptIsFun );
+// // // // console.log( typeof 23 );
+// // // // console.log( typeof "Jonas" );
 
-// // javascriptIsFun = 'YES!';
-// // console.log( typeof javascriptIsFun );
-
-// // // javascriptIsFun = 100;
+// // // javascriptIsFun = 'YES!';
 // // // console.log( typeof javascriptIsFun );
 
-// // let year;
-// // console.log( year );
-// // console.log( typeof year );
+// // // // javascriptIsFun = 100;
+// // // // console.log( typeof javascriptIsFun );
 
-// // year = 1991;
-// // console.log( typeof year );
+// // // let year;
+// // // console.log( year );
+// // // console.log( typeof year );
 
-// // console.log( typeof null );
+// // // year = 1991;
+// // // console.log( typeof year );
 
-// // let age = 30;
-// // age = 31;
+// // // console.log( typeof null );
 
-// // const birthYear = 1991;
-// // // birthYear = 2001;
-// // // const job;
-// // // console.log( age );
+// // // let age = 30;
+// // // age = 31;
 
-// // var job = 'programmer';
-// // job = 'teacher';
+// // // const birthYear = 1991;
+// // // // birthYear = 2001;
+// // // // const job;
+// // // // console.log( age );
 
-// // lastName = 'Schmedtmann';
-// // console.log( lastName );
+// // // var job = 'programmer';
+// // // job = 'teacher';
+
+// // // lastName = 'Schmedtmann';
+// // // console.log( lastName );
+
+// // // const now = 2037;
+// // // const ageJonas = now - 1991;
+// // // const ageSarah = now - 2018;
+// // // console.log( ageJonas, ageSarah );
+
+// // // console.log( ageSarah * 2, ageSarah / 2, 2 ** 3 );
+// // // // 2 ** 3 adalah 3 = 2 * 2 * 2
+
+// // // // const firstName = 'Ryan';
+// // // // const lastName = 'Kresna';
+// // // // console.log( firstName + ' ' + lastName );
+
+// // // // let x = 10 + 5;
+// // // // x += 10;
+// // // // x *= 4;
+// // // // x++;
+// // // // x--;
+// // // // x--;
+// // // // console.log( x );
+
+// // // console.log( ageJonas > ageSarah );
+// // // console.log( ageSarah >= 18 );
+
+// // // const isFullAge = ageSarah >= 18;
+
+// // // console.log( now - 1991 > now - 2018 );
 
 // // const now = 2037;
 // // const ageJonas = now - 1991;
 // // const ageSarah = now - 2018;
-// // console.log( ageJonas, ageSarah );
-
-// // console.log( ageSarah * 2, ageSarah / 2, 2 ** 3 );
-// // // 2 ** 3 adalah 3 = 2 * 2 * 2
-
-// // // const firstName = 'Ryan';
-// // // const lastName = 'Kresna';
-// // // console.log( firstName + ' ' + lastName );
-
-// // // let x = 10 + 5;
-// // // x += 10;
-// // // x *= 4;
-// // // x++;
-// // // x--;
-// // // x--;
-// // // console.log( x );
-
-// // console.log( ageJonas > ageSarah );
-// // console.log( ageSarah >= 18 );
-
-// // const isFullAge = ageSarah >= 18;
 
 // // console.log( now - 1991 > now - 2018 );
 
-// const now = 2037;
-// const ageJonas = now - 1991;
-// const ageSarah = now - 2018;
+// // let x, y;
+// // x = y = 25 - 10 - 5; // x = y = 10
+// // console.log( x, y );
 
-// console.log( now - 1991 > now - 2018 );
+// // const averageAge = ageJonas + ageSarah / 2;
+// // console.log( ageJonas, ageSarah, averageAge );
 
-// let x, y;
-// x = y = 25 - 10 - 5; // x = y = 10
-// console.log( x, y );
+// // const massMark = 78;
+// // const heightMark = 1.69;
+// // const massJohn = 92;
+// // const heightJohn = 1.95;
 
-// const averageAge = ageJonas + ageSarah / 2;
-// console.log( ageJonas, ageSarah, averageAge );
+// // const massMark = 95;
+// // const heightMark = 1.88;
+// // const massJohn = 85;
+// // const heightJohn = 1.76;
 
-// const massMark = 78;
-// const heightMark = 1.69;
-// const massJohn = 92;
-// const heightJohn = 1.95;
+// // const BMIMark = massMark / heightMark ** 2;
+// // const BMIJohn = massJohn / ( heightJohn * heightJohn );
+// // const markHigherBMI = BMIMark < BMIJohn;
 
-// const massMark = 95;
-// const heightMark = 1.88;
-// const massJohn = 85;
-// const heightJohn = 1.76;
+// // console.log( BMIMark, BMIJohn, markHigherBMI );
 
-// const BMIMark = massMark / heightMark ** 2;
-// const BMIJohn = massJohn / ( heightJohn * heightJohn );
-// const markHigherBMI = BMIMark < BMIJohn;
+// const firstName = 'Jonas';
+// const job = 'teacher';
+// const birthYear = 1991;
+// const year = 2037;
 
-// console.log( BMIMark, BMIJohn, markHigherBMI );
+// const jonas = "I'm " + firstName + ', a ' + ( year - birthYear ) + ' year old ' + job + '!';
+// console.log( jonas );
 
-const firstName = 'Jonas';
-const job = 'teacher';
-const birthYear = 1991;
-const year = 2037;
+// const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+// console.log( jonasNew );
 
-const jonas = "I'm " + firstName + ', a ' + ( year - birthYear ) + ' years old ' + job + '!';
-console.log( jonas );
+// console.log( `Just a regular string...` );
+
+// console.log( 'String with \n\
+// multiple \n\
+// lines' );
+
+// console.log( `string
+// multiple
+// lines`);
+
+const age = 10;
+const isOldEnough = age >= 18;
+
+if ( isOldEnough ) {
+    console.log( 'Sarah can start driving license 🚓' );
+} else {
+    const yearLeft = 18 - age;
+    console.log( `Sarah is too young. Wait another ${yearLeft} years :)` );
+}
+
+const birthYear = 3301;
+let century;
+
+if ( birthYear <= 2000 ) {
+    century = 20;
+} else {
+    century = 21;
+}
+
+console.log( century );
