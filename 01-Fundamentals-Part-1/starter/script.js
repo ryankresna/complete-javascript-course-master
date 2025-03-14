177dd80ea38a53ca6d626dd949f9878e7bcde3b8
Ryan_Kresna <ryankresna.n@gmail.com>
2025-03-14 13:51:59 +0700

    javascript

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,124 +1,122 @@
-// // // /*
-// // // let js = "amazing";
-// // // console.log( 40 + 8 + 23 + 10 );
 
-// // // console.log( "Jonas" );
-// // // console.log( 23 );
+// let js = "amazing";
+// console.log( 40 + 8 + 23 + 10 );
 
-// // // let firstName = "Jonas";
+// console.log( "Jonas" );
+// console.log( 23 );
 
-// // // console.log( firstName, js );
-// // // console.log( firstName, js );
-// // // console.log( firstName, js );
+// let firstName = "Jonas";
 
-// // // let jonas_matilda = "JM";
-// // // let $function = 27;
+// console.log( firstName, js );
+// console.log( firstName, js );
+// console.log( firstName, js );
 
-// // // let person = "jonas";
-// // // let PI = 3.1415;
+// let jonas_matilda = "JM";
+// let $function = 27;
 
-// // // let myFirstJob = "Coder";
-// // // let myCurrentJob = "Teacher";
+// let person = "jonas";
+// let PI = 3.1415;
 
-// // // let job1 = "programmer";
-// // // let job2 = "teacher";
+// let myFirstJob = "Coder";
+// let myCurrentJob = "Teacher";
 
-// // // console.log( myFirstJob );
-// // // */
+// let job1 = "programmer";
+// let job2 = "teacher";
 
+// console.log( myFirstJob );
 
-// // // let javascriptIsFun = true;
-// // // console.log( javascriptIsFun );
+// let javascriptIsFun = true;
+// console.log( javascriptIsFun );
 
-// // // // console.log( typeof true );
-// // // console.log( typeof javascriptIsFun );
-// // // // console.log( typeof 23 );
-// // // // console.log( typeof "Jonas" );
+// console.log( typeof true );
+// console.log( typeof javascriptIsFun );
+// console.log( typeof 23 );
+// console.log( typeof "Jonas" );
 
-// // // javascriptIsFun = 'YES!';
-// // // console.log( typeof javascriptIsFun );
+// javascriptIsFun = 'YES!';
+// console.log( typeof javascriptIsFun );
 
-// // // // javascriptIsFun = 100;
-// // // // console.log( typeof javascriptIsFun );
+// javascriptIsFun = 100;
+// console.log( typeof javascriptIsFun );
 
-// // // let year;
-// // // console.log( year );
-// // // console.log( typeof year );
+// let year;
+// console.log( year );
+// console.log( typeof year );
 
-// // // year = 1991;
-// // // console.log( typeof year );
+// year = 1991;
+// console.log( typeof year );
 
-// // // console.log( typeof null );
+// console.log( typeof null );
 
-// // // let age = 30;
-// // // age = 31;
+// let age = 30;
+// age = 31;
 
-// // // const birthYear = 1991;
-// // // // birthYear = 2001;
-// // // // const job;
-// // // // console.log( age );
+// const birthYear = 1991;
+// birthYear = 2001;
+// const job;
+// console.log( age );
 
-// // // var job = 'programmer';
-// // // job = 'teacher';
+// var job = 'programmer';
+// job = 'teacher';
 
-// // // lastName = 'Schmedtmann';
-// // // console.log( lastName );
+// lastName = 'Schmedtmann';
+// console.log( lastName );
 
-// // // const now = 2037;
-// // // const ageJonas = now - 1991;
-// // // const ageSarah = now - 2018;
-// // // console.log( ageJonas, ageSarah );
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log( ageJonas, ageSarah );
 
-// // // console.log( ageSarah * 2, ageSarah / 2, 2 ** 3 );
-// // // // 2 ** 3 adalah 3 = 2 * 2 * 2
+// console.log( ageSarah * 2, ageSarah / 2, 2 ** 3 );
+// 2 ** 3 adalah 3 = 2 * 2 * 2
 
-// // // // const firstName = 'Ryan';
-// // // // const lastName = 'Kresna';
-// // // // console.log( firstName + ' ' + lastName );
+// const firstName = 'Ryan';
+// const lastName = 'Kresna';
+// console.log( firstName + ' ' + lastName );
 
-// // // // let x = 10 + 5;
-// // // // x += 10;
-// // // // x *= 4;
-// // // // x++;
-// // // // x--;
-// // // // x--;
-// // // // console.log( x );
+// let x = 10 + 5;
+// x += 10;
+// x *= 4;
+// x++;
+// x--;
+// x--;
+// console.log( x );
 
-// // // console.log( ageJonas > ageSarah );
-// // // console.log( ageSarah >= 18 );
+// console.log( ageJonas > ageSarah );
+// console.log( ageSarah >= 18 );
 
-// // // const isFullAge = ageSarah >= 18;
+// const isFullAge = ageSarah >= 18;
 
-// // // console.log( now - 1991 > now - 2018 );
+// console.log( now - 1991 > now - 2018 );
 
-// // const now = 2037;
-// // const ageJonas = now - 1991;
-// // const ageSarah = now - 2018;
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
 
-// // console.log( now - 1991 > now - 2018 );
+// console.log( now - 1991 > now - 2018 );
 
-// // let x, y;
-// // x = y = 25 - 10 - 5; // x = y = 10
-// // console.log( x, y );
+// let x, y;
+// x = y = 25 - 10 - 5; // x = y = 10
+// console.log( x, y );
 
-// // const averageAge = ageJonas + ageSarah / 2;
-// // console.log( ageJonas, ageSarah, averageAge );
+// const averageAge = ageJonas + ageSarah / 2;
+// console.log( ageJonas, ageSarah, averageAge );
 
-// // const massMark = 78;
-// // const heightMark = 1.69;
-// // const massJohn = 92;
-// // const heightJohn = 1.95;
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
 
-// // const massMark = 95;
-// // const heightMark = 1.88;
-// // const massJohn = 85;
-// // const heightJohn = 1.76;
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
 
-// // const BMIMark = massMark / heightMark ** 2;
-// // const BMIJohn = massJohn / ( heightJohn * heightJohn );
-// // const markHigherBMI = BMIMark < BMIJohn;
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / ( heightJohn * heightJohn );
+// const markHigherBMI = BMIMark < BMIJohn;
 
-// // console.log( BMIMark, BMIJohn, markHigherBMI );
+// console.log( BMIMark, BMIJohn, markHigherBMI );
 
 // const firstName = 'Jonas';
 // const job = 'teacher';
@@ -141,23 +139,44 @@
 // multiple
 // lines`);
 
-const age = 10;
-const isOldEnough = age >= 18;
+// const age = 10;
+// const isOldEnough = age >= 18;
 
-if ( isOldEnough ) {
-    console.log( 'Sarah can start driving license 🚓' );
-} else {
-    const yearLeft = 18 - age;
-    console.log( `Sarah is too young. Wait another ${yearLeft} years :)` );
-}
+// if ( isOldEnough ) {
+//     console.log( 'Sarah can start driving license 🚓' );
+// } else {
+//     const yearLeft = 18 - age;
+//     console.log( `Sarah is too young. Wait another ${yearLeft} years :)` );
+// }
 
-const birthYear = 3301;
-let century;
+// const birthYear = 3301;
+// let century;
 
-if ( birthYear <= 2000 ) {
-    century = 20;
-} else {
-    century = 21;
-}
+// if ( birthYear <= 2000 ) {
+//     century = 20;
+// } else {
+//     century = 21;
+// }
 
-console.log( century );
+// console.log( century );
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / ( heightJohn * heightJohn );
+// console.log( BMIMark, BMIJohn );
+
+// if ( BMIMark > BMIJohn ) {
+//     console.log( `Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!` );
+// } else {
+//     console.log( `John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!` );
+// } 
+
