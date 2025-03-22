b8e8a304cc357833defaf04a64d417c50d9b97ee
Ryan_Kresna <ryankresna.n@gmail.com>
2025-03-22 23:52:56 +0700

    javascript

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -194,3 +194,13 @@
 // const tips = [calcTip( bills[0] ), calcTip( bills[1] ), calcTip( bills[2] )];
 // const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 // console.log( bills, tips, totals );
+
+const jonas = {
+    firstNam: 'jonas',
+    lastName: 'schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['michael', 'peter', 'steven']
+};
+console.log( jonas );
+
