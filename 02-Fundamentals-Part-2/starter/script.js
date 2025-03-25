501134a05236b721de0fd349f485cdea6587e0f9
Ryan_Kresna <ryankresna.n@gmail.com>
2025-03-25 10:51:53 +0700

    javascript : looping backwards and loops in loops

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -218,3 +218,11 @@ const jonas = [
 for ( let i = jonas.length - 1; i >= 0; i-- ) {
     console.log( i, jonas[i] );
 }
+
+for ( let exercise = 1; exercise < 4; exercise++ ) {
+    console.log( `--- starting exercise ${exercise}` );
+
+    for ( let rep = 1; rep < 6; rep++ ) {
+        console.log( `Exercise ${exercise}: lifting weight repetition ${rep} 🏋️‍♂️` );
+    }
+}
