a354d6b5c07bad6e74da796d837df3cb259fe25c
Ryan_Kresna <ryankresna.n@gmail.com>
2025-04-15 10:51:06 +0700

    76. Selecting and Manipulating Elements new

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -6,5 +6,5 @@ document.querySelector( '.message' ).textContent = 'Correct Number!';
 document.querySelector( '.number' ).textContent = '13';
 document.querySelector( '.score' ).textContent = '10';
 
-console.log( document.querySelector( '.guess' ).value );
 document.querySelector( '.guess' ).value = 23;
+console.log( document.querySelector( '.guess' ).value );
